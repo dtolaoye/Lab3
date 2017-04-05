970c4164821c909290fb1474f77d075dda8cd19c
OLAO2 <OLAO2@128.4.51.223>
2017-04-04 22:29:45 -0400

    pkgEnum, Wild attribute, FiveOfAKind, and Game class

---
+++ BallysBLL/src/main/java/pkgException/DeckException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package pkgException;
+
+
+public class DeckException extends Exception {
+	
+	public DeckException(){
+		super();
+	}
+
+	public DeckException(String message) {
+		super(message);
+		
+	}
+
+}
